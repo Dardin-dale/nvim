c703c648a4ed75f2b4925e289ffd855165d1ad20
Logan Crecraft <logandalec@gmail.com>
2022-10-27 23:54:41 -0700

    fixed font/neovide settings

--- lua/neovide.lua
+++ lua/neovide.lua
@@ -1,3 +1,5 @@
-vim.g.neovide_fullscreen = true
-vim.g.neovide_cursor_vfx_mode = "pixiedust"
-vim.opt.guifont = {"FiraCode\\ NF", ":h10"}
+if vim.g.neovide then
+    vim.g.neovide_fullscreen = true
+    vim.g.neovide_cursor_vfx_mode = "pixiedust"
+    vim.opt.guifont = "FiraCode NF:h10"
+end
