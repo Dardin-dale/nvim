7c1d489043f7f3d3a7874f6a3d8dace5f864466c
Logan Crecraft <logandalec@gmail.com>
2023-02-19 15:12:13 -0800

    adding lsps

--- lua/logan/lsp/lsp-installer.lua
+++ lua/logan/lsp/lsp-installer.lua
@@ -14,6 +14,12 @@ if not status_ok then
 end
 
 
+--[[ local status_ok, jdtls = pcall(require, "jdtls") ]]
+--[[ if not status_ok then ]]
+--[[     return ]]
+--[[ end ]]
+
+
 local lspconfig = require('lspconfig')
 local lsp_defaults = lspconfig.util.default_config
 
@@ -56,16 +62,90 @@ local tsserver_opts = require("logan.lsp.settings.jsonls")
 local rust_analyzer_opts = require("logan.lsp.settings.rust_analyzer")
 local jdtls_opts = require("logan.lsp.settings.jdtls")
 
+
 mason_lspconfig.setup_handlers({
 function ()
     lspconfig.sumneko_lua.setup { on_attach = opts.on_attach, capabilities = opts.capabilities, settings = sumneko_opts }
     lspconfig.tsserver.setup { on_attach = opts.on_attach, capabilities = opts.capabilities }
     lspconfig.rust_analyzer.setup { on_attach = opts.on_attach, capabilities = opts.capabilities, settings = rust_analyzer_opts}
-    lspconfig.jdtls.setup {opts.on_attach, capabilities = opts.capabilities, settings = jdtls_opts, cmd = { 'C:/Program Files/Java/jdt-language-server-1.9.0-202203031534/bin/jdtls' }}
+    lspconfig.jdtls.setup {opts.on_attach, capabilities = opts.capabilities}
     lspconfig.gopls.setup {opts.on_attach, capabilities = opts.capabilities}
+    lspconfig.cssls.setup {opts.on_attach, capabilities = opts.capabilities}
+    lspconfig.html.setup {opts.on_attach, capabilities = opts.capabilities}
 end
 })
 
+
+--[[ local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t') ]]
+--[[]]
+--[[ local workspace_dir = '/path/to/workspace-root/' .. project_name ]]
+
+
+-- See `:help vim.lsp.start_client` for an overview of the supported `config` options.
+--[[ local config = { ]]
+--[[   -- The command that starts the language server ]]
+--[[   -- See: https://github.com/eclipse/eclipse.jdt.ls#running-from-the-command-line ]]
+--[[   cmd = { ]]
+--[[]]
+--[[     -- 💀 ]]
+--[[     'java', -- or '/path/to/java17_or_newer/bin/java' ]]
+--[[             -- depends on if `java` is in your $PATH env variable and if it points to the right version. ]]
+--[[]]
+--[[     '-Declipse.application=org.eclipse.jdt.ls.core.id1', ]]
+--[[     '-Dosgi.bundles.defaultStartLevel=4', ]]
+--[[     '-Declipse.product=org.eclipse.jdt.ls.core.product', ]]
+--[[     '-Dlog.protocol=true', ]]
+--[[     '-Dlog.level=ALL', ]]
+--[[     '-Xms1g', ]]
+--[[     '--add-modules=ALL-SYSTEM', ]]
+--[[     '--add-opens', 'java.base/java.util=ALL-UNNAMED', ]]
+--[[     '--add-opens', 'java.base/java.lang=ALL-UNNAMED', ]]
+--[[]]
+--[[     -- 💀 ]]
+--[[     '-jar', 'C:/Users/Logan/AppData/Local/nvim-data/mason/packages/jdtls/bin', ]]
+--[[          -- ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^                                       ^^^^^^^^^^^^^^ ]]
+--[[          -- Must point to the                                                     Change this to ]]
+--[[          -- eclipse.jdt.ls installation                                           the actual version ]]
+--[[]]
+--[[]]
+--[[     -- 💀 ]]
+--[[     '-configuration', 'C:/Users/Logan/AppData/Local/nvim-data/mason/packages/jdtls/config_win', ]]
+--[[                     -- ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^        ^^^^^^ ]]
+--[[                     -- Must point to the                      Change to one of `linux`, `win` or `mac` ]]
+--[[                     -- eclipse.jdt.ls installation            Depending on your system. ]]
+--[[]]
+--[[]]
+--[[     -- 💀 ]]
+--[[     -- See `data directory configuration` section in the README ]]
+--[[     '-data', workspace_dir ]]
+--[[   }, ]]
+--[[]]
+--[[   -- 💀 ]]
+--[[   -- This is the default if not provided, you can remove it. Or adjust as needed. ]]
+--[[   -- One dedicated LSP server & client will be started per unique root_dir ]]
+--[[   root_dir = require('jdtls.setup').find_root({'.git', 'mvnw', 'gradlew'}), ]]
+--[[]]
+--[[   -- Here you can configure eclipse.jdt.ls specific settings ]]
+--[[   -- See https://github.com/eclipse/eclipse.jdt.ls/wiki/Running-the-JAVA-LS-server-from-the-command-line#initialize-request ]]
+--[[   -- for a list of options ]]
+--[[   settings = { ]]
+--[[     java = { ]]
+--[[     } ]]
+--[[   }, ]]
+--[[]]
+--[[   -- Language server `initializationOptions` ]]
+--[[   -- You need to extend the `bundles` with paths to jar files ]]
+--[[   -- if you want to use additional eclipse.jdt.ls plugins. ]]
+--[[   -- ]]
+--[[   -- See https://github.com/mfussenegger/nvim-jdtls#java-debug-installation ]]
+--[[   -- ]]
+--[[   -- If you don't plan on using the debugger or other eclipse.jdt.ls plugins you can remove this ]]
+--[[   init_options = { ]]
+--[[     bundles = {} ]]
+--[[   }, ]]
+--[[ } ]]
+--[[]]
+--[[ jdtls.start_or_attach(config); ]]
 --[[]]
 --[[ lsp_bridge.setup_handlers({ ]]
 --[[     function (server_name) ]]
