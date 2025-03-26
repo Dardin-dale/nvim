60db663dc2ab730301891f7b022641e1e8e49c5a
Logan Crecraft <loganc@dynotag.com>
2025-03-25 23:53:40 -0700

    session autoload lsp

--- init.lua
+++ init.lua
@@ -2,3 +2,20 @@ require "neovide"
 vim.loader.enable()
 require "logan.plugins"
 require "logan"
+-- BEGIN ANSIBLE MANAGED BLOCK - WSL CLIPBOARD
+-- WSL clipboard integration
+if vim.fn.has('wsl') == 1 then
+  vim.g.clipboard = {
+    name = 'win32yank-wsl',
+    copy = {
+      ['+'] = 'win32yank.exe -i --crlf',
+      ['*'] = 'win32yank.exe -i --crlf',
+    },
+    paste = {
+      ['+'] = 'win32yank.exe -o --lf',
+      ['*'] = 'win32yank.exe -o --lf',
+    },
+    cache_enabled = true,
+  }
+end
+-- END ANSIBLE MANAGED BLOCK - WSL CLIPBOARD
--- lua/logan/sessions.lua
+++ lua/logan/sessions.lua
@@ -1,10 +1,17 @@
-
 local status_ok, sessions = pcall(require, "session_manager")
 if not status_ok then
-	return
+    return
 end
 
 
 sessions.setup {
-    autoload_mode = require('session_manager.config').AutoloadMode.Disabled, -- LastSession, Disabled, CurrentDir
+    autoload_mode = require('session_manager.config').AutoloadMode.CurrentDir,
+    hooks = {
+        after_load = function()
+            -- Force LSP restart after session load
+            vim.defer_fn(function()
+                vim.cmd("LspRestart")
+            end, 1000)
+        end
+    }
 }
