0f6db2e923ee0471ab31a7c4c3e314c508d881d5
Logan Crecraft <loganc@dynotag.com>
2025-03-25 23:24:03 -0700

    one step closer

--- lua/logan/alpha.lua
+++ lua/logan/alpha.lua
@@ -1,35 +1,35 @@
 local status_ok, alpha = pcall(require, "alpha")
 if not status_ok then
-	return
+    return
 end
 
 local status_ok, dash = pcall(require, "alpha.themes.dashboard")
 if not status_ok then
-	return
+    return
 end
 
 dash.section.header.val = {
-	"                                                     ",
-	"  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
-	"  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
-	"  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
-	"  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
-	"  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
-	"  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
-	"                                                     ",
+    "                                                     ",
+    "  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
+    "  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
+    "  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
+    "  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
+    "  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
+    "  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
+    "                                                     ",
 }
 
 dash.section.buttons.val = {
-		dash.button("e", "  New file", "<cmd>ene <CR>"),
-		dash.button("SPC f f", " Find file"),
-		dash.button("SPC f l", "  Recently opened files"),
-		dash.button("SPC f r", "  Frecency/MRU"),
-		dash.button("SPC f g", "󰈬  Find word"),
-		dash.button("SPC f m", "  Jump to bookmarks"),
-		dash.button("SPC f s", "󰥨  Pick Session"),
-		dash.button("SPC s l", "󰝉  Open last session", ":SessionManager load_last_session<cr>"),
-        --[[ dash.button( "s", "  > Settings" , ":e %VIMRC% <CR>"), ]]
-        dash.button( "q", "  Quit NVIM" , ":qa<CR>"),
+    dash.button("e", "  New file", "<cmd>ene <CR>"),
+    dash.button("SPC f f", "  Find file"),
+    dash.button("SPC f l", "  Recently opened files"),
+    dash.button("SPC f r", "  Frecency/MRU"),
+    dash.button("SPC f g", "󰈬  Find word"),
+    dash.button("SPC f m", "  Jump to bookmarks"),
+    dash.button("SPC f s", "󰥨  Pick Session"),
+    dash.button("SPC s l", "󰝉  Open last session", ":SessionManager load_last_session<cr>"),
+    --[[ dash.button( "s", "  > Settings" , ":e %VIMRC% <CR>"), ]]
+    dash.button("q", "  Quit NVIM", ":qa<CR>"),
 }
 
 alpha.setup(dash.config)
