61ecabe190f6490634aabdf14a12c0990c887dd6
Logan Crecraft <loganc@dynotag.com>
2025-03-28 09:00:32 -0700

    misc fixes renew lazy lock

--- lua/logan/debug.lua
+++ lua/logan/debug.lua
@@ -1,17 +1,16 @@
-
 local dap_status_ok, dap = pcall(require, "nvim-dap")
 if not dap_status_ok then
-  return
+	return
 end
 
 local dapui_status_ok, dapui = pcall(require, "dapui")
-if not dap_ui_status_ok then
-  return
+if not dapui_status_ok then
+	return
 end
 
 local dap_text_status_ok, dap_text = pcall(require, "nvim-dap-virtual-text")
 if not dap_text_status_ok then
-  return
+	return
 end
 
 dap.setup()
@@ -19,12 +18,11 @@ dapui.setup()
 dap_text.setup()
 
 dap.listeners.after.event_initialized["dapui_config"] = function()
-  dapui.open()
+	dapui.open()
 end
 dap.listeners.before.event_terminated["dapui_config"] = function()
-  dapui.close()
+	dapui.close()
 end
 dap.listeners.before.event_exited["dapui_config"] = function()
-  dapui.close()
+	dapui.close()
 end
-
