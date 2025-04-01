2236c340bc9c7ac166a98db2324a02636497d4a7
Logan Crecraft <loganc@dynotag.com>
2025-04-01 10:59:26 -0700

    troubleshooting

--- lua/logan/sessions.lua
+++ lua/logan/sessions.lua
@@ -1,6 +1,28 @@
-local status_ok, sessions = pcall(require, "persistance")
+local status_ok, sessions = pcall(require, "persistence")
 if not status_ok then
-    return
+	return
 end
 
-persistance.setup()
+sessions.setup({
+	options = { "buffers", "curdir", "tabpages", "winsize" },
+	pre_save = function()
+		-- Close certain buffers before saving session
+		-- Example: NvimTree, fugitive, etc.
+		vim.cmd([[silent! tabdo silent! NvimTreeClose]])
+	end,
+	-- Exclude file types or buffer types that cause problems
+	-- when restored between sessions
+	exclude = {
+		"NvimTree",
+		"fugitive",
+		"gitcommit",
+		"help",
+		"qf",
+		"netrw",
+		"TelescopePrompt",
+		"terminal",
+		"lazy",
+		"mason",
+		"lspinfo",
+	},
+})
